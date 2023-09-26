70bdf93fd1ab1f5c452f0898ca240962b63e8e8a
Joshua Woods developer <nordev@protonmail.com>
2023-09-26 13:34:47 -0400

    base site

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+ return(
+    <footer>
+    <p>© 2023 Joshua Woods</p>
+  </footer>
+ )
+};
+
+
+
+
+export default Footer;
---
+++ src/components/projects.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <>
+            <section className="projects">
+                <h2>Featured Projects</h2>
+
+                <div className="project-card">
+                    <img src="/path/to/your/image1.jpg" alt="Project 1 description" />
+                    <h3>Project 1 Title</h3>
+                    <p>Short description about Project 1...</p>
+                    <a href="https://link-to-project-1.com" target="_blank" rel="noopener noreferrer">View Project</a>
+                </div>
+
+            </section>
+
+        </>
+    )
+};
+
+
+
+export default Projects;
